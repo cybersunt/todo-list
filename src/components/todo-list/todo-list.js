c5199e74054c7ce22f3ed3a043d5ba9306d722e7
Maksimova, Yuliya <Yuliya.Maksimova@nexign.com>
2020-04-22 17:54:48 +0300

    Передача данных через map

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -2,13 +2,18 @@ import React from 'react';
 import TodoListItem from "../todo-list-item/todo-list-item";
 
 const TodoList = ({ todos }) => {
+
+  const elements = todos.map((item) => {
+    return (
+      <TodoListItem
+        label={item.label}
+        important={item.important}/>
+    )
+  });
+
   return (
     <ul className="list-group todo-list">
-      <TodoListItem label={todos[0].label}/>
-      <TodoListItem
-        label={todos[1].label}
-        important={todos[0].label}/>
-      <TodoListItem label={todos[2].label}/>
+      { elements }
     </ul>
   )
 };
