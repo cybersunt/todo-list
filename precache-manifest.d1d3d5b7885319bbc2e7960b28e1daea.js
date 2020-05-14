064da137519b6e157a4d09274008fbbe078ffd6a
Maksimova, Yuliya <Yuliya.Maksimova@nexign.com>
2020-05-14 11:37:46 +0300

    Updates

---
+++ precache-manifest.d1d3d5b7885319bbc2e7960b28e1daea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16666aebad1450dd7819a265b7bda5c1",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "210683dc23c446959fb7",
+    "url": "/todo-list-react/static/css/main.c3079e98.chunk.css"
+  },
+  {
+    "revision": "4d08608cfbc20e02b1ef",
+    "url": "/todo-list-react/static/js/2.c1d921b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-react/static/js/2.c1d921b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "210683dc23c446959fb7",
+    "url": "/todo-list-react/static/js/main.f8c5180d.chunk.js"
+  },
+  {
+    "revision": "66f196f0dfc693117ab1",
+    "url": "/todo-list-react/static/js/runtime-main.8ec663f4.js"
+  }
+]);
