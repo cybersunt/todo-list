c9dfa61d01c123467035f637d714386df0b8af7f
Maksimova, Yuliya <Yuliya.Maksimova@nexign.com>
2020-05-14 12:04:24 +0300

    Updates

---
+++ precache-manifest.58e74ebaa0e329053b77a3a81921986f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3112e6e0836d42d3be67b21b2a6cf8a",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "f50142963629ec022ff8",
+    "url": "/react-todo-list/static/css/main.c3079e98.chunk.css"
+  },
+  {
+    "revision": "d5ecb6ff8ee1ae3d8808",
+    "url": "/react-todo-list/static/js/2.17bd4e28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.17bd4e28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f50142963629ec022ff8",
+    "url": "/react-todo-list/static/js/main.e45a21d1.chunk.js"
+  },
+  {
+    "revision": "3f55eb4d5430f52ab291",
+    "url": "/react-todo-list/static/js/runtime-main.c6e33228.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todo-list-react/precache-manifest.d1d3d5b7885319bbc2e7960b28e1daea.js"
+  "/react-todo-list/precache-manifest.58e74ebaa0e329053b77a3a81921986f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-list-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-todo-list/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
